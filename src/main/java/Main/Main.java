74107d6d119f731b631f8ead7fff212fd0bfa5de
Vastuolu <naufalnabillansyah49@gmail.com>
2025-05-29 13:33:59 +0700

    add: class Page, method clearPage, and start code in main.java

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,11 +2,33 @@ package Main;
 
 import java.util.HashMap;
 
+import Biz.UserBiz;
+import Page.Page;
+import Util.MissionUtil;
+
 public class Main {
 	public static HashMap<String, String> userCookies = null;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			
+			UserBiz uBiz = new UserBiz();
+			int pilihan;
+			Page.splashScreen();
+			Page.loginRegisterMenu();
+			pilihan = Integer.parseInt(MissionUtil.getUserInput());
+			while(pilihan != 1 || pilihan != 2) {				
+				if(pilihan == 1) {
+					uBiz.login();
+				}else if(pilihan == 2) {
+					uBiz.register();			
+				}else {
+					continue;
+				}
+			}
+		
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
